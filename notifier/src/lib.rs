72e58484779007338674f39d8b3fe05a0469ca20
Jasper Woudenberg <mail@jasperwoudenberg.com>
2018-02-16 11:37:16 +0100

    Add notifier lib for Rust code

--- notifier/src/lib.rs
+++ notifier/src/lib.rs
@@ -1,6 +1,28 @@
-use std::os::raw::c_int;
+extern crate notify;
+
+use notify::{watcher, RecursiveMode, Watcher};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
 #[no_mangle]
-pub extern "C" fn print_string(cb: extern "C" fn() -> c_int) {
+pub extern "C" fn print_string(cb: extern "C" fn() -> ()) {
     println!("hello from rust");
-    cb();
+
+    // Create a channel to receive the events.
+    let (tx, rx) = channel();
+
+    // Create a watcher object, delivering debounced events.
+    // The notification back-end is selected based on the platform.
+    let mut watcher = watcher(tx, Duration::from_secs(10)).unwrap();
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher
+        .watch("/Users/jasper/Desktop", RecursiveMode::Recursive)
+        .unwrap();
+
+    match rx.recv() {
+        Ok(_event) => cb(),
+        Err(e) => println!("watch error: {:?}", e),
+    };
 }
